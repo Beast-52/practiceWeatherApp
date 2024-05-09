a93d41b568b69788708872d54c2ad70f620be07d
Muhammad Zain <114417655+Zain180@users.noreply.github.com>
2024-05-09 19:36:23 +0500

    js file updated

--- script.js
+++ script.js
@@ -1,16 +1,86 @@
 const apikey = "8b93c561a9e0abbf3574fad3704c4aa7";
-let cityname = "Bahawalpur";
+let cityname = "Murree";
+let data;
+const cardGroups = document.querySelector(".group_cards");
+const city = document.querySelector(".Weather_country");
+let search = document.querySelector(".search-input");
+let form = document.querySelector(".search-form");
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apikey}`;
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let searchValue = search.value;
+  cityname = searchValue || "Bahawalpur";
+  fetchdata(); // fetch data every time the form is submitted
+});
 
 async function fetchdata() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metric&appid=${apikey}`;
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    console.log("Data:", data);
+    data = await response.json();
+    city.innerHTML = data.name;
+    const clutter = `<div class="card">
+        <div class="card_top">
+          <h2>Temperature</h2>
+        </div>
+        <img class="Weather_img" src="./images/1.png" alt="" />
+        <div class="card_bottom">
+          <h1 class="temp_a_value">${data.main.temp} &deg;C</h1>
+          <span class="temp">Temperature is <span class="temp_value"></span></span>
+          <span class="min_temp">Min Temperature is <span class="min_temp_value">${
+            data.main.temp_min
+          }&deg;C</span></span>
+          <span class="max_temp">Max Temperature is <span class="max_temp_value">${
+            data.main.temp_max
+          }&deg;C</span></span>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card_top">
+          <h2>Humidity</h2>
+        </div>
+        <img class="Weather_img" src="./images/2.jpeg" alt="" />
+        <div class="card_bottom">
+          <h1 class="humidity_a_value">${data.main.humidity} %</h1>
+          <span class="wind_deg">Wind Degree is <span class="wind_deg_value">${
+            data.wind.deg
+          }</span></span>
+          <span class="feel_like">Feel Like <span class="feel_like_value">${
+            data.main.feels_like
+          }</span></span>
+          <span class="humidity">Humidity<span class="humidity_value"></span></span>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card_top">
+          <h2>Wind info</h2>
+        </div>
+        <img class="Weather_img" src="./images/3.png" alt="" />
+        <div class="card_bottom">
+          <h1 class="wind_info_a_value">${data.wind.speed} km/h</h1>
+          <span class="sun_rise_time">Sunrise Time is <span class="sun_rise_time_value">${convertUnixToTime(
+            data.sys.sunrise
+          )}</span></span>
+          <span class="sun_set_time">Sunset Time is <span class="sun_set_time_value">${convertUnixToTime(
+            data.sys.sunset
+          )}</span></span>
+        </div>
+      </div>`;
+
+    cardGroups.innerHTML = clutter;
   } catch (error) {
-    console.log(`Err in fetching api ${error}`);
+    console.log(`Error in fetching API: ${error}`);
   }
 }
 
 fetchdata();
+
+
+function convertUnixToTime(unixTimestamp) {
+  const date = new Date(unixTimestamp * 1000);
+  const hours = date.getHours();
+  const minutes = "0" + date.getMinutes();
+  return hours + ":" + minutes.slice(-2);
+}
